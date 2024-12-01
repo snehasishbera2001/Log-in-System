f1290c6ca5d481c6cf770f3faaa1c4f7ac9248b4
Snehasish Bera <snehasish.bera2001@gmail.com>
2024-12-01 06:32:14 -0800

    Add files via upload

---
+++ snehasish/contactus.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Contact Us</title>
+</head>
+
+<body>
+<nav class="topheading">
+            <!-- Navbar Brand-->
+            <a class="about"><b>Contact Us</b></a>
+            <!-- Sidebar Toggle-->
+
+            <!-- Navbar Search-->
+          
+        </nav>
+</body>
+</html>
